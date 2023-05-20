7988b64408330f10db39594771bb58f64ca05d0b
the-geek-deep <deepumrajkar15@gmail.com>
2023-05-20 14:58:05 +0530

    handled the routing

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+// User signup route
+router.post('/signup', authController.signup);
+
+// User login route
+router.post('/login', authController.login);
+
+module.exports = router;
---
+++ routes/posts.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const postController = require('../controllers/postController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Home page route
+router.get('/', postController.getAllPosts);
+
+// Create a new blog post route
+router.post('/create', authMiddleware.authenticateToken, postController.createPost);
+
+// Edit a blog post route
+router.put('/edit/:postId', authMiddleware.authenticateToken, postController.editPost);
+
+// Delete a blog post route
+router.delete('/delete/:postId', authMiddleware.authenticateToken, postController.deletePost);
+
+module.exports = router;
