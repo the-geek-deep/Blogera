3a7faaa479a0a304c5e80a96af8b61cebafddad7
the-geek-deep <deepumrajkar15@gmail.com>
2023-05-20 21:39:18 +0530

    added middleware

---
+++ middleware/middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+import dotnenv from 'dotenv';
+dotnenv.config();
+const secretKey = process.env.SECRET_KEY;
+
+// Authenticate request using JWT token
+exports.authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  jwt.verify(token,  (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+};
